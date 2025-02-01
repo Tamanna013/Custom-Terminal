5cf85a69a7187ae92c9eb44b20012a96fccc6f2b
TashxMG <149794391+Tamanna013@users.noreply.github.com>
2025-02-01 15:26:05 +0530

    Update Terminal.c

--- Terminal.c
+++ Terminal.c
@@ -9,102 +9,214 @@
 #include <sys/stat.h>
 
 #define MAX_INPUT 1024
-#define MAX_PATH 256
 
 void dateTime(){
-	time_t tt = time(NULL);
+    time_t tt = time(NULL);
     printf("%s", ctime(&tt));
 }
 
-void clear(){
+void clearScr(){
     #ifdef _WIN32
-        system("cls"); // Windows
+        system("cls");
     #else
-        system("clear"); // Linux/Mac
+        system("clear");
     #endif
-    	printf("\033[31m"); // Red text color
+    printf("\033[31m");  // Red text
+    printf("\033[0m");   // Reset color
 }
 
 void startscr(){
-    clear();
+    clearScr();
     printf("Welcome to Tamanna's Terminal! Let's make some progress today!\n");
-	dateTime();
-	printf("\nType \"help\" if you need help to figure out things in this terminal!\n\n");
+    dateTime();
+    printf("\nType \"help\" if you need help!\n\n");
 }
 
 void help(){
-    printf("\n");
-    printf("You can enter the following commands:\n\n");
-    printf("clr         >>> To clear the screen\n");
-    printf("dateTime    >>> To display date and time\n");
-    printf("startscr    >>> To show start screen\n");
-    printf("ls          >>> To list files\n");
-    printf("mkdir <dir> >>> To create a new folder\n");
-    printf("cd <dir>    >>> To change directory\n");
-    printf("exit        >>> To exit\n");
-    printf("\n");
+    printf("\nYou can enter the following commands:\n\n");
+    printf("clear       >>> Clear the screen\n");
+    printf("dateTime    >>> Display date and time\n");
+    printf("startscr    >>> Show start screen\n");
+    printf("ls          >>> List files\n");
+    printf("cd <dir>    >>> Change directory\n");
+    printf("mkdir <dir> >>> Create a directory\n");
+    printf("rf <file>   >>> Remove a file\n");
+    printf("read <file> >>> Read and display file content\n");
+    printf("write <file>>> Write to a file\n");
+    printf("append <file> >>> Append content to a file\n");
+    printf("mv <file> <new_name> >>> Rename or move a file\n");
+    printf("copy <source> <destination> >>> Copy a file\n");
+    printf("touch <file> >>> Create an empty file\n");
+    printf("ccount <file> >>> Count characters in a file\n");
+    printf("lknow <file> >>> Count lines in a file\n");
+    printf("cknow <file> <char> >>> Count occurrences of a character\n");
+    printf("exit        >>> Exit\n\n");
 }
 
 void ls() {
     DIR *dir;
     struct dirent *entry;
 
-    dir = opendir("."); // Opens the current directory using opendir().
+    dir = opendir(".");
     if (dir == NULL) {
         perror("Unable to open directory");
         return;
     }
 
     printf("Files in the current directory:\n");
-    while ((entry = readdir(dir)) != NULL) { // Reads entries one by one using readdir() and prints their names.
+    while ((entry = readdir(dir)) != NULL) {
         printf("%s\n", entry->d_name);
-	}
-    closedir(dir); // Closes the directory stream with closedir().
+    }
+    closedir(dir);
 }
 
-void mkdir(const char *dirname) {
-    if(mkdir(dirname, 0777) == 0){ // mkdir(dirname, 0777) == 0 means "Did making the folder succeed?"
-        printf("Directory '%s' created successfully.\n", dirname);
+void cd(char *path) {
+    if (path == NULL || strcmp(path, "") == 0) {
+        path = getenv("HOME");
     }
-	else{
-        perror("Error creating directory");
-    }
-}
-
-void cd(const char *path) {
-    if(chdir(path) == 0){ // System function chdir() to change the current working directory.
+    if (chdir(path) == 0) {
         printf("Changed directory to: %s\n", path);
-    }
-	else{
+    } else {
         perror("Error changing directory");
     }
 }
 
-void exit(){
-	printf("\nExiting the terminal...");
-    exit(1);
+void mymkdir(const char *input) {
+    char dirname[MAX_INPUT] = "";
+    sscanf(input, "mkdir %[^\n]", dirname); // Extract everything after 'mkdir'
+
+    if (strlen(dirname) == 0) {
+        printf("Usage: mkdir <directory_name>\n");
+        return;
+    }
+
+    if (mkdir(dirname, 700) == 0) {
+        printf("Directory '%s' created successfully.\n", dirname);
+    } else {
+        perror("Error creating directory");
+    }
 }
 
-int main(){
-	char input[MAX_INPUT];
-	char command[MAX_INPUT];
-    char argument[MAX_INPUT];
-	startscr();
-	while(1){
-		printf("Input>> ");
-    	fgets(input, MAX_INPUT, stdin);
-        input[strcspn(input, "\n")] = '\0'; // Remove newline character
-    	
-    	sscanf(input, "%s %[^\n]", command, argument);
-    	
-		if(strcmp(, "clear") == 0) clear();
-		else if(strcmp(a, "help") == 0) help();
-		else if(strcmp(a, "dateTime") == 0) dateTime();
-		else if(strcmp(a, "startscr") == 0) startscreen();
-		else if(strcmp(command, "ls") == 0) ls();
-        else if (strcmp(command, "cd") == 0) cd(argument);
-        else if (strcmp(command, "mkdir") == 0) mkdir(argument);
-		else if(strcmp(a, "exit") == 0) exit();
-		else printf("Unknown command: Refer to the \"help\" function");
-	}
+void append(char *filename) {
+    FILE *file = fopen(filename, "a");
+    if (!file) {
+        perror("Error opening file");
+        return;
+    }
+    char content[MAX_INPUT];
+    printf("Enter content to append to '%s':\n", filename);
+    fgets(content, MAX_INPUT, stdin);
+    content[strcspn(content, "\n")] = '\0';  // Remove newline
+    fprintf(file, "%s\n", content);
+    fclose(file);
+    printf("Content appended to '%s'.\n", filename);
+}
+
+void mv(char *oldname, char *newname) {
+    if (rename(oldname, newname) == 0) {
+        printf("Renamed '%s' to '%s'.\n", oldname, newname);
+    } else {
+        perror("Error renaming file");
+    }
+}
+
+void copy(char *src, char *dest) {
+    FILE *fsrc = fopen(src, "r");
+    if (!fsrc) {
+        perror("Error opening source file");
+        return;
+    }
+    FILE *fdest = fopen(dest, "w");
+    if (!fdest) {
+        perror("Error opening destination file");
+        fclose(fsrc);
+        return;
+    }
+    char ch;
+    while ((ch = fgetc(fsrc)) != EOF) {
+        fputc(ch, fdest);
+    }
+    fclose(fsrc);
+    fclose(fdest);
+    printf("Copied '%s' to '%s'.\n", src, dest);
+}
+
+void ccount(char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Error opening file");
+        return;
+    }
+    int count = 0;
+    while (fgetc(file) != EOF) count++;
+    fclose(file);
+    printf("Total characters in '%s': %d\n", filename, count);
+}
+
+void lknow(char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Error opening file");
+        return;
+    }
+    int count = 0;
+    char line[MAX_INPUT];
+    while (fgets(line, sizeof(line), file)) count++;
+    fclose(file);
+    printf("Total lines in '%s': %d\n", filename, count);
+}
+
+void cknow(char *filename, char c) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Error opening file");
+        return;
+    }
+    int count = 0;
+    char ch;
+    while ((ch = fgetc(file)) != EOF) {
+        if (ch == c) count++;
+    }
+    fclose(file);
+    printf("Character '%c' appears %d times in '%s'.\n", c, count, filename);
+}
+
+int main() {
+    char input[MAX_INPUT];
+    char *args[MAX_INPUT / 2 + 1];
+    startscr();
+
+    while (1) {
+        printf("Input>> ");
+        if (!fgets(input, MAX_INPUT, stdin)) {
+            continue;
+        }
+        input[strcspn(input, "\n")] = '\0'; // Remove newline
+
+        int argc = 0;
+        char *token = strtok(input, " ");
+        while (token) {
+            args[argc++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[argc] = NULL;
+
+        if (argc == 0) continue;  // Ignore empty input
+
+        if (strcmp(args[0], "clear") == 0) clearScr();
+        else if (strcmp(args[0], "help") == 0) help();
+        else if (strcmp(args[0], "dateTime") == 0) dateTime();
+        else if (strcmp(args[0], "startscr") == 0) startscr();
+        else if (strcmp(args[0], "ls") == 0) ls();
+        else if (strcmp(args[0], "cd") == 0) cd(args[1]);
+        else if (strcmp(args[0], "mkdir") == 0) mymkdir(argc, args);
+        else if (strcmp(args[0], "append") == 0) append(args[1]);
+        else if (strcmp(args[0], "mv") == 0 && argc > 2) mv(args[1], args[2]);
+        else if (strcmp(args[0], "copy") == 0 && argc > 2) copy(args[1], args[2]);
+        else if (strcmp(args[0], "ccount") == 0) ccount(args[1]);
+        else if (strcmp(args[0], "lknow") == 0) lknow(args[1]);
+        else if (strcmp(args[0], "cknow") == 0 && argc > 2) cknow(args[1], args[2][0]);
+        else if (strcmp(args[0], "exit") == 0) exit(0);
+        else printf("Unknown command!\n");
+    }
 }
